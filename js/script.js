9440203649f0333223fe023700aab41a58f62292
Sondre N <49595556+sono81@users.noreply.github.com>
2020-08-19 17:00:54 +0200

    mess, stuck

--- js/script.js
+++ js/script.js
@@ -9,16 +9,32 @@ fetch('https://raw.githubusercontent.com/sono81/semester-project-2/master/json/c
         console.log(error);
     });
 
-    let wrapper = document.querySelector('.wrapper');
+const wrapper = document.querySelector('.wrapper');
+const playerSelectInfo = document.querySelector('.info');
+const card = document.querySelector('.character');
+let charactersSelected = 0;
 
 function getData(characters) {
+    var cards = '';
+    playerSelectInfo.innerHTML += `<h1>Select character</h1>
+                                    <p>Player${charactersSelected+1}</p>`
     for (i = 0; i < characters.length; i++) {
-        wrapper.innerHTML +=   `<div class="character">
-                                    <img src="../images/characters/${characters[i].Id+'.png'}">
-                                    <p>${characters[i].Name}</p>
-                                    <p>${characters[i].Culture}</p>
-                                </div>`
-
-                                console.log(characters[i]);
+        cards += `<div class="character" id="character${i}" onclick="selectCard(this, ${characters[i].Id})">
+                                     <img src="../images/characters/${characters[i].Id+'.png'}">
+                                     <div class="info">
+                                         <p class="name">${characters[i].Name}</p>
+                                         <p class="culture">${characters[i].Culture}</p>
+                                     </div>
+                                 </div>`
     }
-};
+    document.querySelector('.wrapper').innerHTML = cards;
+}
+
+//function selectCard(card, charactersSelected){
+//    card.style.border = '5px solid green';
+//
+//    if (card.style.border == '5px solid green') {
+//        playerSelectInfo.innerHTML = `<h1>Select character</h1>
+//                                    <p>Player2</p>`
+//    }
+//}
