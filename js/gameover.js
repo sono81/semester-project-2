903c96e890b1990d4eabe5d170eef34caffe1e8f
Sondre N <49595556+sono81@users.noreply.github.com>
2020-09-07 12:40:35 +0200

    Add line to show when player two wins

--- js/gameover.js
+++ js/gameover.js
@@ -7,7 +7,6 @@ if (localStorage.getItem('winner') === 'Player 1') {
     winnerName.innerHTML += localStorage.getItem('winner');
     winnerImage.innerHTML += '<img src="../images/characters/' + localStorage.getItem('player_one_id') + '.png">';
 } else {
+    winnerName.innerHTML += localStorage.getItem('winner');
     winnerImage.innerHTML += '<img src="../images/characters/' + localStorage.getItem('player_two_id') + '.png">';
 };
-
-winnerImage.innerHTML += '<img src="/images/characters/' + localStorage.getItem('player_one_id') + '.png />';
